87775aa1ac6d9e68718877b5bc5f5f0bf9826f29
Kade-95 <ikeka95@gmail.com>
2022-01-12 07:58:25 +0100

    [master]: Export Storage Class

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,12 @@ import { Collection } from "./collections/collection.class";
 import { Database } from "./databases/database.class";
 import { Document } from "./documents/document.class";
 import { IDocument } from "./documents/document.interface";
+import { Storage } from "./storage/storage.class"
 
-export { Database, Collection, Document, IDocument };
+export {
+    Database,
+    Collection,
+    Document,
+    IDocument,
+    Storage
+};
