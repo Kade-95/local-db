861e1370920759a2b8a8c008d6a8007fcea0f7e6
Kade-95 <ikeka95@gmail.com>
2024-04-12 16:56:47 +0100

    export engine type

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Collection } from "./collections/collection.class";
 import { Database } from "./databases/database.class";
 import { Document } from "./documents/document.class";
 import { IDocument } from "./documents/document.interface";
+import { EngineTypes } from "./models/engine.types";
 import { CodeStorage } from "./storage/code-storage.class"
 
 export {
@@ -9,5 +10,6 @@ export {
     Collection,
     Document,
     IDocument,
-    CodeStorage
+    CodeStorage,
+    EngineTypes
 };
